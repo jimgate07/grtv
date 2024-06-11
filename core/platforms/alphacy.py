4b1ccca2d05548f1fb12a9c80d4dd13a0b671f42
jimgate07 <51256740+jimgate07@users.noreply.github.com>
2024-06-11 16:13:16 +0300

    Update alphacy.py

--- core/platforms/alphacy.py
+++ core/platforms/alphacy.py
@@ -2,7 +2,7 @@ import sys
 import subprocess
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", bs4])
-
+from bs4 import BeautifulSoup
 import requests
 import re
 
